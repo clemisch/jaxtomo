5efd38466c28c7893806c6a2031d79164f3beb2a
Clemens Schmid <clem.schmid@tum.de>
2023-01-09 18:59:33 +0100

    Update fan FPB example

--- examples/fan_fbp.py
+++ examples/fan_fbp.py
@@ -3,22 +3,12 @@ import scipy.ndimage as nd
 from skimage.data import shepp_logan_phantom
 from skimage.transform import radon
 import jax
-import jax.numpy as jnp
 
-import sys
-sys.path.append("/users/schmid/phd/code/other/jax_projector")
+from jaxtomo import fan_fp as P_fp
+from jaxtomo import fan_bp as P_bp
+from jaxtomo import proj_filter
+from jaxtomo import util
 
-import fan_fp as P_fp
-import fan_bp as P_bp
-import proj_filter
-import util
-
-util.set_cuda_device(2)
-util.set_preallocation(False)
-
-###############################################################################
-# 2D PARALLEL
-###############################################################################
 
 img = shepp_logan_phantom()
 N = 256
@@ -30,25 +20,21 @@ S = -1000
 D = 1000
 M = (D - S) / abs(S)
 
-angles = linspace(0, 2 * pi, N * 2, False)
-projs = P_fp.get_projs_2d(vol, angles, 1., N, M, 10, 1., S, D).block_until_ready()
-
+n_angles = N * 2
+angles = linspace(0, 2 * pi, n_angles, False)
+projs = P_fp.get_projs_2d(vol, angles, 1., N, M, 10, 1., S, D)
 vol_bp = P_bp.get_bp(projs, angles, M, 1., N, 16, 1., S, D)
-
-
-
-
 
 w = proj_filter.gen_fbp_weights(N)
 projs_f = proj_filter.proj_filter(projs, w)
-
 fbp = P_bp.get_bp(projs_f, angles, M, 1., N, 16, 1., S, D)
+fbp = fbp / n_angles
 
 
-
-T = exp(-projs / 50)
-ncounts = 1e4
-T_noise = poisson(T * ncounts) / ncounts
-mu_noise = -log(T_noise) * 50
-
-fbp_noise = P_bp.get_bp(proj_filter.proj_filter(mu_noise, w), angles, M, 1., N, 16, 1., S, D)
+fig, ax = subplots(1, 2, figsize=(10, 5))
+ax[0].imshow(vol[0], vmin=0, vmax=1)
+ax[1].imshow(fbp[:, 7], vmin=0, vmax=1)
+ax[0].set_title("Original")
+ax[1].set_title("FBP")
+tight_layout()
+show()
