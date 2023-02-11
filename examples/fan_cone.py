3a9bff7fda17e6ffb6a913018d3007a53ede759c
Clemens Schmid <clem.schmid@tum.de>
2023-02-11 10:39:57 +0100

    Improve fan/cone comparison example

--- examples/fan_cone.py
+++ examples/fan_cone.py
@@ -1,27 +1,24 @@
 from pylab import *
-import scipy.ndimage as nd
-from skimage.data import shepp_logan_phantom
-import jax
 
 from jaxtomo import fan_fp
 from jaxtomo import cone_fp
-
 from jaxtomo import util
+
 util.set_preallocation(False)
 util.set_cuda_device(2)
 
-fname = "/buffer/schmid/forbild_head.npz"
-
-vol = array(load(fname)["arr_0"])
-vol = vol.transpose(1, 0, 2)
 vol_sh_x = 256
-vol = vol / vol_sh_x
-vol = nd.zoom(vol, vol_sh_x / vol.shape[0], order=1)
-vol = jax.device_put(vol)
-nslices = vol.shape[0]
+nslices = vol_sh_x
 
-z_source = 350
-z_det = 2000
+vol = zeros((vol_sh_x, vol_sh_x, vol_sh_x), dtype="float32")
+vol[
+    nslices//4 : nslices//4 + nslices//2,
+    vol_sh_x//4 : vol_sh_x//4 + vol_sh_x//2,
+    vol_sh_x//4 : vol_sh_x//4 + vol_sh_x//2
+] = 1
+
+z_source = vol_sh_x
+z_det = z_source * 10
 M = (z_det + z_source) / abs(z_source)
 
 n_angles = vol_sh_x * 2
@@ -54,12 +51,14 @@ proj_cone = cone_fp.get_fp(
 )
 
 
-fig, ax = subplots(1, 3, figsize=(14, 5))
-ax[0].imshow(vol[nrows//2])
-ax[1].imshow(proj_fan[:vol_sh_x, nrows//5 * 2])
-ax[2].imshow(proj_cone[:vol_sh_x, nrows//5 * 2])
-ax[0].set_title("Original")
-ax[1].set_title("Sinogram (fan beam)")
-ax[2].set_title("Sinogram (cone beam)")
+fig, ax = subplots(2, 3, figsize=(14, 8))
+ax[0, 0].imshow(vol[nrows//2])
+ax[0, 1].imshow(proj_fan[:vol_sh_x, nrows//4])
+ax[0, 2].imshow(proj_cone[:vol_sh_x, nrows//4])
+ax[0, 0].set_title("Original")
+ax[0, 1].set_title("Sinogram (fan beam)")
+ax[0, 2].set_title("Sinogram (cone beam)")
+ax[1, 1].imshow(proj_fan[n_angles//16])
+ax[1, 2].imshow(proj_cone[n_angles//16])
 tight_layout()
 show()
