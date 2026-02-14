f35648ebd59bbfe41823df93644310899c6a72ad
Clemens Schmid <clemisch@tuta.io>
2026-02-14 14:39:06 +0100

    Optimize cone FP principal-direction handling

--- jaxtomo/projectors/cone_fp.py
+++ jaxtomo/projectors/cone_fp.py
@@ -5,6 +5,23 @@ from functools import partial
 from ..util import multi_vmap, interp2d, jaxmap
 
 
+def _rotate_xy_90(vol):
+    return jnp.transpose(vol, (0, 2, 1))
+
+
+def _select_rotated_volume(rot_idx, vol0, vol1, vol2, vol3):
+    return jax.lax.switch(
+        rot_idx,
+        (
+            lambda _: vol0,
+            lambda _: vol1,
+            lambda _: vol2,
+            lambda _: vol3,
+        ),
+        operand=None
+    )
+
+
 def _get_ray(vol, theta, u, v, xx, yy, zz, s, d):
     # pixel coords
     Dx = d * jnp.cos(theta) - u * jnp.sin(theta)
@@ -50,46 +67,22 @@ def _get_ray(vol, theta, u, v, xx, yy, zz, s, d):
     return ray
 
 
-@partial(jax.jit, static_argnames=("U", "V"))
-def _get_fp_angle(vol, theta, dX, U, dU, V, dV, s, d, princ_dir):
-    dZ = dX  # cubic voxels
-    Z = vol.shape[0]
-    X = vol.shape[1]
+@jax.jit
+def _get_fp_angle(vol0, vol1, vol2, vol3, theta, xx_base, zz, uu, vv, s, d, princ_dir):
+    rot_idx = princ_dir - 1
+    theta = theta - rot_idx * jnp.pi / 2
 
-    # width in px/vx => one px/vx smaller than physical volume/detector!
-    width_img = dX * (X - 1)
-    height_img = dZ * (Z - 1)
-    width_proj = dU * (U - 1)
-    height_proj = dV * (V - 1)
+    # Mapping from principal direction to axis orientation.
+    xx_sign = jnp.where(rot_idx < 2, 1.0, -1.0).astype(xx_base.dtype)
+    yy_sign = jnp.where(
+        jnp.logical_or(rot_idx == 0, rot_idx == 3),
+        1.0,
+        -1.0
+    ).astype(xx_base.dtype)
 
-    # origins
-    O_X = dX * (-0.5 * X + 0.5)
-    O_Z = dZ * (-0.5 * Z + 0.5)
-    O_U = dU * (-0.5 * U + 0.5)
-    O_V = dV * (-0.5 * V + 0.5)
-
-    # axes for volume and projector
-    xx = jnp.linspace(0., 1., X, endpoint=True) * width_img + O_X
-    yy = jnp.linspace(0., 1., X, endpoint=True) * width_img + O_X
-    zz = jnp.linspace(0., 1., Z, endpoint=True) * height_img + O_Z
-    uu = jnp.linspace(0., 1., U, endpoint=True) * width_proj + O_U
-    vv = jnp.linspace(0., 1., V, endpoint=True) * height_proj + O_V
-
-    # handle principal direction
-    nrots = princ_dir - 1
-    theta = theta - nrots * jnp.pi / 2
-
-    def body_fun(i, arg):
-        xx, yy, vol = arg
-        vol = jnp.transpose(vol, (0, 2, 1))
-        xx, yy = yy, -xx
-        return xx, yy, vol
-
-    xx, yy, vol = jax.lax.fori_loop(
-        0, nrots,
-        body_fun,
-        (xx, yy, vol)
-    )
+    xx = xx_sign * xx_base
+    yy = yy_sign * xx_base
+    vol = _select_rotated_volume(rot_idx, vol0, vol1, vol2, vol3)
 
     # map over all pixels to get one projection
     get_proj = multi_vmap(
@@ -119,13 +112,42 @@ _get_princ_dirs = jax.vmap(_get_princ_dir)
 
 @partial(jax.jit, static_argnames=("U", "V"))
 def get_fp(vol, thetas, dX, U, dU, V, dV, s, d):
-    
+    dZ = dX  # cubic voxels
+    Z = vol.shape[0]
+    X = vol.shape[1]
+
+    # width in px/vx => one px/vx smaller than physical volume/detector!
+    width_img = dX * (X - 1)
+    height_img = dZ * (Z - 1)
+    width_proj = dU * (U - 1)
+    height_proj = dV * (V - 1)
+
+    # origins
+    O_X = dX * (-0.5 * X + 0.5)
+    O_Z = dZ * (-0.5 * Z + 0.5)
+    O_U = dU * (-0.5 * U + 0.5)
+    O_V = dV * (-0.5 * V + 0.5)
+
+    xx_base = jnp.linspace(0., 1., X, endpoint=True) * width_img + O_X
+    zz = jnp.linspace(0., 1., Z, endpoint=True) * height_img + O_Z
+    uu = jnp.linspace(0., 1., U, endpoint=True) * width_proj + O_U
+    vv = jnp.linspace(0., 1., V, endpoint=True) * height_proj + O_V
+
+    # Precompute all principal-direction rotations once per FP call.
+    vol0 = vol
+    vol1 = _rotate_xy_90(vol0)
+    vol2 = _rotate_xy_90(vol1)
+    vol3 = _rotate_xy_90(vol2)
+
     princ_dirs = _get_princ_dirs(thetas)
 
     # map over angles to get full FP
     def mapfun(args):
         theta, princ_dir = args
-        return _get_fp_angle(vol, theta, dX, U, dU, V, dV, s, d, princ_dir)
+        return _get_fp_angle(
+            vol0, vol1, vol2, vol3,
+            theta, xx_base, zz, uu, vv, s, d, princ_dir
+        )
 
     projs = jaxmap(mapfun, (thetas, princ_dirs), unroll=1)
 
