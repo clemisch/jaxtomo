daa04d1ea2a876908aa6b2c197dd58e160da0da5
Clemens Schmid <clemens.schmid@esrf.fr>
2026-05-10 18:53:27 +0200

    register triton FP/BP as transposes

---
+++ jaxtomo/custom_vjps/cone_fp_bp_triton.py
@@ -0,0 +1,37 @@
+import jax
+
+from jaxtomo.projectors import cone_bp_triton
+from jaxtomo.projectors import cone_fp_triton
+
+
+def get_fp_bp(angles, X, dX, Y, U, dU, V, dV, S, D):
+    @jax.custom_vjp
+    def project(vol):
+        proj = cone_fp_triton.get_fp(vol, angles, dX, U, dU, V, dV, S, D)
+        return proj
+
+    def _project_fwd(vol):
+        return project(vol), None
+
+    def _project_bwd(res, g):
+        return (backproject(g),)
+
+    project.defvjp(_project_fwd, _project_bwd)
+
+    @jax.custom_vjp
+    def backproject(proj):
+        vol = cone_bp_triton.get_bp(proj, angles, dU, dV, X, Y, dX, S, D)
+        return vol
+
+    def _backproject_fwd(proj):
+        return backproject(proj), None
+
+    def _backproject_bwd(res, g):
+        return (project(g),)
+
+    backproject.defvjp(_backproject_fwd, _backproject_bwd)
+
+    project = jax.jit(project)
+    backproject = jax.jit(backproject)
+
+    return project, backproject
